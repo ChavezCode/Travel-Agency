5a322a8572aaa2ed924ca6eefead71d72b78bdf0
unknown <jch1248@wgu.edu>
2024-10-07 10:14:54 -0700

    purchase service and purchase response service classes created

---
+++ src/main/java/com/example/demo/services/PurchaseResponse.java
@@ -0,0 +1,11 @@
+package com.example.demo.services;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PurchaseResponse {
+
+    private String OrderTrackingNumber;
+}
